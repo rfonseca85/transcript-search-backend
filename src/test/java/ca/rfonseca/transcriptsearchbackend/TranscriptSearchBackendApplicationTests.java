b97b8bef4bdd9e87807fd3bfe8ec617e83f5cd90
Rafael Fonseca <rafael.fonseca@GTO00027m.local>
2021-11-21 16:29:06 -0500

    Adding spring boot boilerplate

---
+++ src/test/java/ca/rfonseca/transcriptsearchbackend/TranscriptSearchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.rfonseca.transcriptsearchbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranscriptSearchBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
